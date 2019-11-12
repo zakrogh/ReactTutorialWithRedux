3c0f5eff9f4fbb77c7ec5732830eae7e3066d96c
Student <example@example.com>
2019-11-12 10:42:09 -0800

    finished react tutorial

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,13 +1,21 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 function Ticket(props){
   return (
-      <div>
-        <h3>{props.location} - {props.names}</h3>
-        <p><em>{props.issue}</em></p>
-        <hr/>
-      </div>
+    <div className="color-toggle">
+        <style global jsx>{`
+          div .color-toggle{
+            background-color: red;
+          }
+          .color-toggle:hover{
+            background: green;
+          }
+        `}</style>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
+    </div>
   );
 }
 
