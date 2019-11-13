0746f8b2053af4936e493de4fa30db8acd30fdcc
Student <example@example.com>
2019-11-13 15:12:15 -0800

    add error 404 page route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ function Header(){
     <div>
       <h1>Help Queue</h1>
       <img src={wikipedia}/>
+      <img src='../assets/images/1.webp'/>
       <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
     </div>
   );
