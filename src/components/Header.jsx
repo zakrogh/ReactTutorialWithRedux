6fb5e3cdaaa9cf726c16cc1132cf21817cb1e47e
Student <example@example.com>
2019-11-19 15:59:26 -0800

    remove img link from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,6 @@ function Header(){
     <div>
       <h1>Help Queue</h1>
       <img src={wikipedia}/>
-      <img src='../assets/images/1.webp'/>
       <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
     </div>
   );
